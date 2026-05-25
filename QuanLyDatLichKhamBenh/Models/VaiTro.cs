65f4d6b0d7ff9573c965a7d86891d88266767874
ShinChill <huynhvinh1209@gmail.com>
2026-05-25 15:56:29 +0700

    cap nhat cau truc thu muc

--- QuanLyDatLichKhamBenh/Models/VaiTro.cs
+++
@@ -1,9 +0,0 @@
-namespace QuanLyDatLichKhamBenh.Models
-{
-    public static class VaiTro
-    {
-        public const string QuanTriVien = "QuanTriVien";
-        public const string BacSi = "BacSi";
-        public const string BenhNhan = "BenhNhan";
-    }
-}
