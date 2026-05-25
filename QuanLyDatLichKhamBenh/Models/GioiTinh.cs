f59cb2577ad147b2914b3a2d3d37b2410c3bc64b
hoangvinh660 <hoangvinh660@users.noreply.github.com>
2026-05-25 15:56:29 +0700

    cap nhat cau truc thu muc

--- QuanLyDatLichKhamBenh/Models/GioiTinh.cs
+++
@@ -1,9 +0,0 @@
-namespace QuanLyDatLichKhamBenh.Models
-{
-    public static class GioiTinh
-    {
-        public const string Nam = "Nam";
-        public const string Nu = "Nu";
-        public const string Khac = "Khac";
-    }
-}
